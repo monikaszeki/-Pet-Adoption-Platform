be48dd40c3188a94fddb20f66e31eb13c6aff6d9
Monika <macbookpro@macbook.local>
2024-07-01 08:38:06 +0200

    auth change

--- resources/views/pets/show.blade.php
+++ resources/views/pets/show.blade.php
@@ -1,32 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-body">
-                        @if($pets && $pets->isNotEmpty())
-                            <h2>My pets</h2>
+    @auth
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            @if($pets && $pets->isNotEmpty())
+                                <h2>My pets</h2>
                                 <ul class="list-group">
                                     @foreach($pets as $pet)
                                         <li class="list-group-item d-flex">
-                                             <div class="p-0 m-0 flex-grow-1">
-                                                  {{ $pet->name  }}
-                                             </div>
-                                             <a class="btn btn-outline-primary btn-sm" style="margin-right: 5px" href="{{ url(route('updatePetList', ['petId' => $pet->id])) }}">Edit</a>
-                                             <a class="btn btn-outline-primary btn-sm" href="{{ url(route('delete', ['petId' => $pet->id])) }}">Delete</a>
+                                            <div class="p-0 m-0 flex-grow-1">
+                                                {{ $pet->name  }}
+                                            </div>
+                                            <a class="btn btn-outline-primary btn-sm" style="margin-right: 5px" href="{{ url(route('updatePetList', ['petId' => $pet->id])) }}">Edit</a>
+                                            <a class="btn btn-outline-primary btn-sm" href="{{ url(route('delete', ['petId' => $pet->id])) }}">Delete</a>
                                         </li>
                                     @endforeach
                                 </ul>
-                        @else
-                            I don't have any pets!
-                        @endif
+                            @else
+                                <p>I don't have any pets!</p><br>
+                            @endif
                             <a class="btn btn-primary btn-sm" style="margin-top: 15px;" href="{{ url(route('createPetList'))}}">Add new pet</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endauth
 @endsection
 
